bad13489973be908c697fdea5d9769f731f1e871
Zeinep123 <ownblack77@gmail.com>
2024-11-21 21:53:23 +0500

    RenamedMain.java

--- RenamedMain.java
+++ RenamedMain.java
@@ -1,4 +1,4 @@
-public class Main {
+public class RenamedMain {
     public static void main(String[] args) {
         ExamChecking checker1 = new ExamChecking(6);
         ExamChecking checker2 = new ExamChecking(4);
